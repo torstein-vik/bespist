f227cbb072e24d69ea57beffe5c53117b90ccae7
torstein-vik <torsteinv64@gmail.com>
2017-05-08 12:02:19 +0200

    Added bunch of stuff on IT2 exam

---
+++ src/index.js
@@ -0,0 +1,420 @@
+// init-function
+$(function(){
+
+    // Load all the products into the DOM and hide all tabs
+    loadProducts().done(() => {
+        // Hide all tabs
+        $(".selectionelement").hide();
+
+        // Initialises the system responsible for tabs
+        initSelectionSystem();
+
+
+        // Load the specified URL
+        loadURL(url);
+
+
+        // Show the previously hidden body (this is to prevent ugly loading)
+        $("body").show();
+    });
+
+
+    // Initialises the system responsible for booking rooms
+    initRoomBuying();
+
+    // Refresh the basket-page
+    refreshBasket();
+
+    // On submit method for complete purchase form
+    $("#completepurchase").on('submit', SendBestilling);
+
+    // Start the slideshow
+    startSlideshow();
+
+});
+
+function loadURL(url){
+    switch(url){
+        case "":
+            $("#home").show();
+            $("#subject > div[for=home]").addClass("active");
+            break;
+        case "home":
+            $("#home").show();
+            $("#subject > div[for=home]").addClass("active");
+            break;
+        case "products":
+            $("#products").show();
+            $("#subject > div[for=products]").addClass("active");
+            break;
+        case "cart":
+            $("#purchase").show();
+            $("#subject > div[for=purchase]").addClass("active");
+            break;
+    }
+}
+
+
+// Initialises the system responsible for tabs
+function initSelectionSystem(){
+     // When a tab button is clicked...
+    $(".selection > div").click(function(event){
+
+        // Select the div that should open when this tab button is pressed
+        var tab = $("#" + $(this).attr("for"));
+
+        // If it is active from before...
+        if($(this).hasClass("active")){
+
+            // Then unactivate the button
+            $(this).removeClass("active");
+
+            // And slide up
+            tab.slideUp();
+
+        } else {
+            // Else do the following:
+
+            // Make this the active tab
+            $(this).addClass('active');
+
+            // If there is another active tab...
+            if($(this).parent().has('.active').length > 0){
+
+                // Make all those other tab buttons inactive
+                $(this).siblings().removeClass('active');
+
+                // Retrive the id of the tab-button parent, i.e. which 'group' of tabs it is from
+                var parentid = $(this).parent().attr('id');
+
+
+                // Using this id, slide up the corresponing tabs (takes aprox. 1 second), and when done...
+                $(".selectionelement[for=" + parentid + "]").slideUp().promise().done(() => {
+
+                    // and if this tab-button is still active (no other tab button has been clicked during the animation),
+                    if($(this).hasClass('active')){
+
+                        // Slide down the correct tab
+                        tab.slideDown();
+                    }
+                });
+
+            } else {
+                // Else just slide down the tab
+                tab.slideDown();
+
+            }
+
+
+        }
+
+    });
+}
+
+// Load all the products into the DOM
+function loadProducts(){
+    var def = new $.Deferred();
+    $.ajax({
+        url:"api.php?type=products"
+    }).done((json) => {
+        var products = JSON.parse(json);
+        console.log(products);
+
+
+        // For all products (see products.json), do...
+        products.forEach((product, index) => { // product is the product and index is the index of the product in the array
+
+            // Add the tab-button with opening product<index> and with product.name as text
+            $("#order-selection").append("<div id='button"+index+"' for='product"+index+"'>"+product.name+"</div>");
+
+            // Add the actual tab div, with product.name as title and product.description as a description.
+            $("#order").append("<div class='selectionelement' for='order-selection' id='product"+index+"'><h2>"+product.name+"</h2>"+product.description+"<p></div>");
+
+            // Add the order button, which opens a div with a form (using tab-system)
+            $("#product"+index).append('<div class="selection" id="order'+index+'"><div for="order'+index+'-content" style="width:100%;">Bestill ('+product.price+' kr)</div></div>');
+
+            // Add the div containing the order-form which is opened by the order button.
+            $("#product"+index).append('<div for="order'+index+'" class="selectionelement" id="order'+index+'-content"><form></form></div>');
+
+            // Add various input-fields to the form above
+            $("#order"+index+"-content > form").append('Hvilken dato vil du gå?');
+            $("#order"+index+"-content > form").append('<input type="date" id="date'+index+'"><br>');
+            $("#order"+index+"-content > form").append('<input type="submit" value="Legg til i handlekurv!"><br>');
+
+            // Put 'today' into the date field
+            $("#date" + index).get(0).valueAsDate = new Date();
+
+            // When this form is submitted...
+            $("#order"+index+"-content > form").on('submit', (e) => {
+
+                // Stop the browser from actually sending the form somewhere
+                e.preventDefault();
+
+                // Retrive the date
+
+                var date = $("#date" + index)[0].valueAsDate;
+
+                // Make sure the date is in the future
+                if(date < new Date()){
+                    alert("Du må bestille noe i framtiden!");
+                    return;
+                }
+
+                // Make sure only two on the same day, and only one of each.
+
+                // Load in the basket
+                var basket = JSON.parse(localStorage.getItem("basket")) || {content: []};
+
+                // Find all product-orders that have the same date, and make sure there are fewer than two.
+                if(basket.content.filter((ob) => new Date(ob.date).toDateString() == date.toDateString() && ob.type == "product").length >= 2){
+                    alert("Du har brukt opp alle våre guider den dagen! Venneligst velg en annen dato.");
+                    return;
+                }
+
+                // Find all product-orders that have the same date and are for the same product, and make sure there are fewer than one.
+                if(basket.content.filter((ob) => ob.type == "product" && new Date(ob.date).toDateString() == date.toDateString() && ob.product == index ).length >= 1){
+                    alert("Du har allerede reservert den turen den dagen. Venneligst velg en annen dato.");
+                    return;
+                }
+
+                // Add the order to the basket
+                addToBasket({type: "product", date: date, product: index});
+
+                // Simulate a click on the product-tab to close it
+
+                $("#button"+index).click();
+
+            });
+        });
+
+        def.resolve();
+    });
+
+    return def;
+
+}
+
+// Starts the slideshow
+function startSlideshow(){
+
+    // Variable to keep track of which slide is being displayed
+    var slide = -1;
+
+    // Function that makes the program go to the next slide
+    nextSlide = function(){
+        // Increment the slide index by one, and if it tips over the gallery length, then bring it back to zero.
+        slide = (slide + 1) % gallery.length;
+
+        // Change the src of the slide image to the next one
+        $("#slide").attr('src', gallery[slide].path);
+
+        // Change the text of the image to the next one
+        $("#slide_text").html(gallery[slide].text);
+    }
+
+    // Call the function once first to have an image from the get-go
+    nextSlide();
+
+    // Then call it every 4 seconds
+    setInterval(nextSlide, 4000);
+}
+
+// Add an object to the basket
+function addToBasket(object){
+    // Either load the basket from local storage, or create a new one
+
+    var basket = JSON.parse(localStorage.getItem("basket")) || {content:[]};
+
+    // Add this order to the basket
+
+    basket.content.push(object);
+
+    // And add the basket to the local storage
+
+    localStorage.setItem("basket", JSON.stringify(basket));
+
+    // Refresh the basket-page
+
+    refreshBasket();
+}
+
+// Initialises the system responsible for booking rooms
+function initRoomBuying(){
+    // Make the date today by default
+    $("#roomform > input[type=date]")[0].valueAsDate = new Date();
+
+    // Select the input that provides the number of guests, and when it's value is changed...
+    $("#roomform > input[type=number]").on('change', function(){
+        // update the price on the submit button
+        $("#roomform > input[type=submit]").val("Legg til i handlekurv (" + (roomprice*$(this).val()) + " kr)")
+    });
+
+    // When the room form is submitted...
+    $("#roomform").on('submit', function(e){
+
+        // Stop the browser from actually doing something with the form
+        e.preventDefault();
+
+        // Pull out the date and amount of persons from the form
+        var date = $("#roomform > input[type=date]")[0].valueAsDate;
+        var amt  = $("#roomform > input[type=number]")[0].valueAsNumber;
+
+        // Make sure amt > 0
+        if(amt < 1){
+            alert("Du må bestille for minst en person!");
+            return;
+        }
+
+        // Make sure date is a friday
+        // new Date(2017, 0, 6) is the first friday this year, done this way so that it will work in different locales (e.g. the US, where sunday is the first day of the week)
+        if(date.getDay() != new Date(2017, 0, 6).getDay()){
+            alert("Du må bestille for en fredag!");
+            return;
+        }
+
+        // Make sure the date is in the future
+        if(date < new Date()){
+            alert("Du må bestille noe i framtiden!");
+            return;
+        }
+
+        // Add the order to the basket
+        addToBasket({type: "room", date: date, amt: amt});
+
+        // Simulate a click on the tab to close it
+        $("#room-button").click();
+    });
+}
+
+// Refresh the basket-page
+function refreshBasket(){
+    $("#basket tbody").html("");
+
+    // Load the basket
+    var basket = JSON.parse(localStorage.getItem("basket")) || {content: []};
+
+    // For calculating the total price
+    var totalprice = 0;
+
+    // A list of all the rows in the created table, used for removing items.
+    var rows = [];
+
+    // For each element...
+    basket.content.forEach((order, index) => {
+        // Create a row
+        row = $("<tr> </tr>");
+        rows[index] = row;
+
+        // Extract the name, which is either a product or a room
+        var name = order.type == "room" ? "Rom" : products[order.product].name;
+
+        // Extract the date
+        var date = new Date(order.date).toDateString();
+
+        // Extract the amount in case of room, and - else.
+        var amt = order.type == "room" ? order.amt : "-";
+
+        // Calculate price
+        var price = order.type == "room" ? order.amt * roomprice : products[order.product].price;
+
+        // Add price to total price
+        totalprice += price;
+
+        // Create a button to remove an item from the basket
+        var remove = $("<td class='remove'> X </td>");
+
+        // If this button is clicked, remove the associated row
+        remove.click(() => {
+            if(window.confirm("Er du sikker?")){
+                rows[index].remove();
+                basket.content.splice(index, 1);
+                localStorage.setItem("basket", JSON.stringify(basket));
+
+            }
+        });
+
+
+        // Append all these to the row
+        row.append("<td>" + name + "</td>");
+        row.append("<td>" + date + "</td>");
+        row.append("<td>" + amt + "</td>");
+        row.append("<td>" + price + "</td>");
+        row.append(remove);
+
+        // Append the row to the baket table
+        $("#basket tbody").append(row);
+    });
+
+    $("#totprice").html(totalprice);
+}
+
+// Collects all the data, empties the basket, and emails the data.
+function SendBestilling(e){
+    // Stop javascrpt from actually submitting anything
+    e.preventDefault();
+
+    // Collect from the form:
+    var name = $("#completepurchase > #name").val();
+    var address = $("#completepurchase > #address").val();
+    var message = $("#completepurchase > #message").val();
+
+    // Forming the email:
+    var email = "The following order came in: \n\nName:\n" + name + "\n\nAddress:\n"+address+"\n\nMessage:\n" + message + "\n\nOrders:";
+
+
+    // Load the basket
+    var basket = JSON.parse(localStorage.getItem("basket"));
+
+    // Make sure that went okay
+    if(!basket){
+        return;
+    }
+
+    // Total price
+    var totalprice = 0;
+
+    // For each element...
+    basket.content.forEach((order, index) => {
+        email += "\n";
+
+        // Extract the name, which is either a product or a room
+        var name = order.type == "room" ? "Rom" : products[order.product].name;
+
+        // Extract the date
+        var date = new Date(order.date).toDateString();
+
+        // Extract the amount in case of room, and - else.
+        var amt = order.type == "room" ? order.amt : "-";
+
+        // Calculate price
+        var price = order.type == "room" ? order.amt * roomprice : products[order.product].price;
+
+        // Add price to total price
+        totalprice += price;
+
+        // Add all to email
+        email += name + ", " + date + (order.type == "room" ? ", "+amt+" personer" : "") + ", til en pris på " + price
+    });
+
+    email += "\n\nTotal pris: \n" + totalprice;
+
+    // Sending it
+    SendEpost(email);
+
+    // Empty the basket
+    localStorage.setItem("basket", JSON.stringify({content:[]}));
+    refreshBasket();
+
+    // Empty the fields
+    var name = $("#completepurchase > #name").val("");
+    var address = $("#completepurchase > #address").val("");
+    var message = $("#completepurchase > #message").val("");
+
+    // Alert user
+    alert("Gratulerer! Bestillingen har blitt sendt!")
+}
+
+// Simulated version of sending an email. Just prints it to console for now.
+function SendEpost(email){
+    console.log(email);
+}
--- src/index.php
+++ src/index.php
@@ -1,118 +1,95 @@
-<?php
-    session_start();
-
-    $conn = new mysqli("localhost", "root", "");
-    if ($conn->connect_errno) {
-        printf("Connect failed: %s\n", $mysqli->connect_error);
-        return;
-    }
-
-    $conn -> set_charset("utf8");
-
-    $pages = [
-        "home" => [
-            "page" => "home.php",
-            "script" => "home.js",
-            "scriptinit" => "init",
-            "css" => "home.css",
-            "navid" => 0
-        ],
-        "404" => [
-            "page" => "notfound.php",
-            "navid" => -1
-        ]
-    ];
-
-    if (array_key_exists("page", $_GET)){
-        $page = $_GET["page"];
-    } else {
-        $page = "home";
-    }
-
-    if (isset($pages[$page])){
-        $pageo = $pages[$page];
-    } else {
-        $pageo = $pages[404];
-    }
-
-    $auth = isset($_SESSION["user"])
-?>
 <html>
     <head>
         <title>Bespist</title>
         <meta charset="utf-8" />
-        <link rel="icon" href="res/favicon.png">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans" />
-        <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
+
+        <script src="lib/jquery-3.2.1.min.js"></script>
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Oxygen"    rel="stylesheet" type="text/css">
+
         <link rel="stylesheet" href="index.css"/>
 
-        <?php
-            if(isset($pageo["script"])){
-                echo "<script src='".$pageo["script"]."'></script>";
-            }
+        <script src="gallery.json"> </script>
+        <script src="index.js"> </script>
+        <script>
+        var url = "<?php echo (isset($_GET["page"]) ? $_GET["page"] : "");?>";
+        </script>
 
-            if(isset($pageo["scriptinit"])){
-                ?>
-                <script>
-                    $(<?php echo $pageo["scriptinit"];?>);
-                </script>
-                <?php
-            }
-
-            if(isset($pageo) and isset($pageo["css"])){
-                echo "<link rel='stylesheet' href='".$pageo["css"]."'/>";
-            }
-        ?>
     </head>
-    <body>
-        <div id="content">
-            <header>
-                <div id="logo">
-                    <a href='?page=home'><img src="res/logo_transparent_text.png" alt="Faroe Adventures"></a>
+    <body style="display: none;">
+        <div id="global">
+
+            <div id="header">
+
+                <h1 class="title" id="title"> Bespist </h1>
+
+
+                <div class="selection" id="subject">
+                    <div for="home">Hjem</div>
+                    <div for="order">Turer</div>
+                    <div for="finish">Handlekurv</div>
                 </div>
-                <nav id="mainNav">
-                    <ul>
-                        <li navid=0><a href="/?page=home"> Home </a></li>
-                        <li navid=1><a href="/?page=attractions"> Food </a></li>
-                        <li navid=2><a href="/?page=contact"> Contact </a></li>
-                    </ul>
-                </nav>
-                <nav id="userNav">
-                    <ul>
-                        <?php
-                        if($auth) {
-                            ?>
-                            <li navid=3><a style="padding-left: 60px;" href='/?page=mypage'> My page </a></li>
-                            <li navid=4><a href='/?page=logout'> Log out </a></li>
-                            <?php
-                        } else {
-                            ?>
-                            <li navid=5><a style="padding-left: 60px;" href='/?page=register'> Register </a></li>
-                            <li navid=6><a href='/?page=login'> Log in </a></li>
-                            <?php
-                        }
-                        ?>
-                    </ul>
-                </nav>
+            </div>
 
-                <script>
-                    var navid = <?php echo $pageo["navid"];?>;
+            <div id="content">
+                <div id="home" for="subject" class="selectionelement">
 
-                    $("ul > li[navid='" + navid + "'] > a").addClass('active');
-                </script>
-            </header>
-            <main>
-                <?php
-                    include $pageo["page"];
-                ?>
-            </main>
-            <footer>
-                <div>&copy; Torstein Vik 2017</div>
-            </footer>
+                    <img id="slide">
+                    <div id="slide_text"></div><p><br>
+
+                    Skal du på ferie til Sunnmøre? Her hos Villa Norangdal spesialiserer vi i guidede turer og overnatting!<p><br>
+                    Vi organiserer turer til Kvitegga, Slogen, Blæja, og Skruven. Vi driver på året rundt, og på vinteren bruker vi ski! <p><br>
+                </div>
+                <div id="order" for="subject" class="selectionelement">
+                    Vi tilbyr overnatting og turer. Du kan kjøpe disse uavhengig av hverandre, men om du vil overnatte samme helg som du går tur, pass på å velge riktige datoer! Klikk på knappen under for å se overnattingsmuligheter, og på knappene under der igjen for turtilbud. Overnatting inkluderer så klart mat, og koster 2800kr per person.<p>
+                    <div class="selection" id="rooms">
+                        <div for="room" id="room-button" style="width:100%;"> Overnattingsalternativ</div>
+                    </div>
+                    <div id="room" class="selectionelement" for="rooms">
+                        <form id="roomform">
+                            Velg den fredagen dere ønsker skiweekend:
+                            <input type="date">
+                            Hvor mange personer er dere?
+                            <input type="number" value=1 min=0>
+                            <input type="submit" value="Legg til i handlekurv (2800 kr)">
+                        </form>
+                    </div>
+
+                    Her er våre fantastiske turtilbud:<br><p>
+
+                    <div class="selection" id="order-selection">
+                    </div>
+                </div>
+                <div id="finish" for="subject" class="selectionelement">
+                    <div id="basket">
+                        <table>
+                            <thead>
+                                <td>Navn</td>
+                                <td>Dato</td>
+                                <td>Antall</td>
+                                <td>Pris</td>
+                                <td>Avbestill</td>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+
+                        <div>Total pris: <span id="totprice"> </span> kr</div>
+
+                        <h2>Fullfør kjøpet</h2>
+
+                        <form id="completepurchase">
+                            <input type="text" id="name" placeholder = "Navn">
+                            <input type="text" id="address" placeholder = "Adresse">
+                            <textarea rows=5 id="message" placeholder="Valgfri melding til oss"></textarea>
+                            <input type="submit" value="Gjennomfør!">
+                        </form>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </body>
 </html>
-<?php
-    $conn->close();
-?>
