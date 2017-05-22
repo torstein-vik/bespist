bbfd01322246ae828a3e5ee2b7c823a111dd0b12
Torstein Vik <torsteinv64@gmail.com>
2017-05-23 00:16:01 +0200

    Added system for registering

--- src/api.php
+++ src/api.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    $conn = new mysqli("localhost", "root", "","");
+    $conn = new mysqli("localhost", "root", "","bespist");
     if ($conn->connect_errno) {
         printf("Connect failed: %s\n", $mysqli->connect_error);
         return;
@@ -13,90 +13,157 @@
 
 
 
-    if (isset($_GET["type"])){
-        $type = $_GET["type"];
+    if (!isset($_GET["type"])){
+        return;
+    }
 
-        if ($type == "checklogin"){
-            if ($auth){
-            ?>
-                {
-                    "status": 1,
-                    "username": "<?php echo $_SESSION["user"]?>"
-                }
-            <?php
-            } else {
-            ?>
-                {
-                    "status": 0
-                }
-            <?php
+    $type = $_GET["type"];
+
+    if ($type == "checklogin"){
+        if ($auth){
+        ?>
+            {
+                "status": 1,
+                "username": "<?php echo $_SESSION["user"]?>"
             }
-        } else if ($type == "logout"){
-            unset($_SESSION["user"]);
-            ?>
-                {
-                    "status": 1
-                }
-            <?php
-        } else if ($type == "products"){
-            /*$products = $conn->query("SELECT * FROM products");
-
-            echo "[";
-            foreach($products as $product){
-
+        <?php
+        } else {
+        ?>
+            {
+                "status": 0
             }
-            echo "]";*/
-            ?>
-                [
-                    {
-                        "name": "Kvitegga",
-                        "description": "Denne siden er under bearbeidelse. Turen tar 9 timer.",
-                        "price": 2500
-                    },
-                    {
-                        "name": "Slogen",
-                        "description": "Denne siden er under bearbeidelse. Turen tar 8 timer.",
-                        "price": 2200
-                    },
-                    {
-                        "name": "Blæja",
-                        "description": "Bli med på en gruppetur til Blæja! ",
-                        "price": 1500
-                    },
-                    {
-                        "name": "Skruven",
-                        "description": "Denne siden er under bearbeidelse. Turen tar 5 timer.",
-                        "price": 1700
-                    }
-                ]
-            <?php
-        } else if ($type == "login"){
-            $username = $_POST["username"];
-            $password = $_POST["password"];
+        <?php
+        }
+        return;
+    } else if ($type == "logout"){
+        unset($_SESSION["user"]);
+        unset($_SESSION["userid"]);
+        ?>
+            {
+                "status": 1
+            }
+        <?php
+        return;
+    } else if ($type == "products"){
+        /*$products = $conn->query("SELECT * FROM products");
 
-            $_SESSION["user"] = $username;
+        echo "[";
+        foreach($products as $product){
+
+        }
+        echo "]";*/
+        ?>
+        [
+            {
+                "name": "Kvitegga",
+                "description": "Denne siden er under bearbeidelse. Turen tar 9 timer.",
+                "price": 2500
+            },
+            {
+                "name": "Slogen",
+                "description": "Denne siden er under bearbeidelse. Turen tar 8 timer.",
+                "price": 2200
+            },
+            {
+                "name": "Blæja",
+                "description": "Bli med på en gruppetur til Blæja! ",
+                "price": 1500
+            },
+            {
+                "name": "Skruven",
+                "description": "Denne siden er under bearbeidelse. Turen tar 5 timer.",
+                "price": 1700
+            }
+        ]
+        <?php
+    } else if ($type == "login"){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $_SESSION["user"] = $username;
+
+        ?>
+            {
+                "success": true,
+                "message": "OK"
+            }
+        <?php
+        return;
+    } else if ($type == "register"){
+        $username = $_POST["username"];
+        $password1 = $_POST["password1"];
+        $password2 = $_POST["password2"];
+
+        if(!(isset($username) && $username != "" && isset($password1) && $password1 != "" && isset($password2) && $password2 != "")){
 
             ?>
                 {
-                    "success": true,
-                    "message": "OK"
+                    "success": false,
+                    "message": "Vennligst fyll ut alle feltene"
                 }
             <?php
-        } else if ($type == "register"){
-            $username = $_POST["username"];
-            $password = $_POST["password1"];
-            $password = $_POST["password2"];
+            return;
 
-            $_SESSION["user"] = $username;
-
-            ?>
-                {
-                    "success": true,
-                    "message": "OK"
-                }
-            <?php
         }
 
+        if($password1 != $password2){
+            ?>
+            {
+                "success": false,
+                "message": "Passordene samsvarer ikke!"
+            }
+            <?php
+            return;
+        }
+
+        $username  = $conn->escape_string($username);
+        $password1 = $conn->escape_string($password1);
+        $password2 = $conn->escape_string($password2);
+
+        $username_ok = $conn->query("SELECT * FROM users WHERE username = '$username'")->num_rows == 0;
+
+        if(!$username_ok){
+            ?>
+            {
+                "success": false,
+                "message": "Brukernavnet er i bruk fra før!"
+            }
+            <?php
+            return;
+        }
+
+        $salt = bin2hex(random_bytes(32));
+
+        $hash = hash('sha256', $password1.$salt);
+
+        $privilege = "'user'";
+
+        $query = $conn->query("INSERT INTO `users`  (username, passhash, passsalt, privilege) VALUES ('".$username."',0x".$hash.",0x".$salt.",".$privilege.");");
+
+        if($query){
+            $userid = $conn->query("SELECT users.userid FROM `mitt-feriested`.`users` WHERE username = '".$username."'")->fetch_assoc();
+
+            $_SESSION['userid'] = $userid["userid"];
+            $_SESSION['user'] = $username;
+
+            ?>
+            {
+                "success": true,
+                "message": "OK"
+            }
+            <?php
+            return;
+        } else {
+            ?>
+            {
+                "success": false,
+                "message": "Ukjent feil"
+            }
+            <?php
+            return;
+        }
+
+        return;
     }
 
 
