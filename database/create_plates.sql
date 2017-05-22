850cab3f0ef5a4924f85850bc2fa5d93a943e281
Torstein Vik <torsteinv64@gmail.com>
2017-05-22 22:36:16 +0200

    Ran node-file to produce plates-sql

---
+++ database/create_plates.sql
@@ -0,0 +1,27 @@
+
+-- SQL script for adding attractions
+
+-- If you get Error Code 1175, Go to Edit -> Preferences -> "SQL Editor" -> "Other" -> uncheck "Safe Updates". Then go to Query -> Reconnect to Server
+
+USE `bespist`;
+
+-- Deleting all previous entries
+SET FOREIGN_KEY_CHECKS=0;
+DELETE FROM `plates`;
+SET FOREIGN_KEY_CHECKS=1;
+
+-- Reseting auto_increment
+ALTER TABLE `plates` AUTO_INCREMENT = 1;
+
+
+
+-- Adding attractions
+INSERT INTO `bespist` (name, content, previewimg, price, category) VALUES ('reker", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "100", "starter'),
+                                                                          ('reker1", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "100", "starter'),
+                                                                          ('reker2", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "432", "starter'),
+                                                                          ('reker3", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "214", "main'),
+                                                                          ('reker4", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "6542", "main'),
+                                                                          ('reker5", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "6524", "main'),
+                                                                          ('reker6", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "54600", "dessert'),
+                                                                          ('reker7", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "600", "dessert'),
+                                                                          ('reker8", "Nystekte reker i kokende olje, med hvitløk. Blir det bedre?", "/res/placeholder.png", "900", "dessert');
